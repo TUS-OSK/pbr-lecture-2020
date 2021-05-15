9993c5f8e838e8a5b08f2f49a217856ef7a2f2da
yumcyawiz <hashdolf1923@gmail.com>
2021-05-16 00:25:36 +0900

    add pi constants

--- ref/src/constant.h
+++ ref/src/constant.h
@@ -2,5 +2,8 @@
 #define _CONSTANT_H
 
 constexpr float PI = 3.14159265359f;
+constexpr float PI_MUL_2 = 2.0f * PI;
+constexpr float PI_INV = 1.0f / PI;
+constexpr float PI_MUL_2_INV = 1.0f / PI_MUL_2;
 
 #endif
